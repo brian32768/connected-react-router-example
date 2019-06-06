9fe159af0dcd22ce37fdb46e59fb206ed085e456
Brian H Wilson <brian@wildsong.biz>
2019-06-05 20:48:03 -0700

    trying to make query latlon work

--- src/components/MapWrapper.js
+++ src/components/MapWrapper.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Map from './Map'
+import { connect } from 'react-redux'
 
 // Keep some center location here as our default
 // until some other value is loaded in from the url
@@ -12,21 +14,29 @@ const state = {
 const MapWrapper = ({query}) => {
     console.log("MapWrapper", query);
     try {
-        if (query.lon) {
-            state.lon = query.lon;
-        }
-        if (query.lat) {
-            state.lat = query.lat;
-        }
+        const lat = Number(query.lat)
+        const lon = Number(query.lon)
+        if (lon) state.x = lon;
+        if (lat) state.y = lat;
     } catch(err) {
         console.log("err=", err);
     }
     return (
-      <div>
+      <>
         <h1>Map</h1>
         <Map lat={ state.y } lon={ state.x } />
-      </div>
+      </>
     )
 }
 
-export default MapWrapper
+MapWrapper.propTypes = {
+  query: PropTypes.object,
+}
+
+// These props are coming from the Redux state,
+
+const mapStateToProps = state => ({
+  query: state.router.location.query,
+})
+
+export default connect(mapStateToProps)(MapWrapper)
